2be14c5a0e57dd6410904cd271691db7220b671d
jakrzus <jakub.krzus@gmail.com>
2024-10-10 22:03:01 +0200

    [create] Trips::Stats::WeeklyStats

---
+++ app/domain/trips/stats/weekly_stats.rb
@@ -0,0 +1,19 @@
+module Trips
+  module Stats
+    class WeeklyStats
+      def total_distance
+        trips.sum(&:distance)
+      end
+
+      def total_price
+        trips.sum(&:price)
+      end
+
+      private
+
+      def trips
+        @_trips ||= Trip.where(date: Date.current.all_week)
+      end
+    end
+  end
+end
