2bee7a764efc161c0be6d788300f8fc116b709e5
jakrzus <jakub.krzus@gmail.com>
2024-10-11 00:41:25 +0200

    [add] db level validations to trips

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -3,11 +3,11 @@
 # Table name: trips
 #
 #  id                  :bigint           not null, primary key
-#  date                :date
-#  destination_address :string
-#  distance            :integer
-#  price               :decimal(8, 2)
-#  start_address       :string
+#  date                :date             not null
+#  destination_address :string           not null
+#  distance            :integer          not null
+#  price               :decimal(8, 2)    not null
+#  start_address       :string           not null
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #
