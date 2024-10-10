d208fc6fc065d80081dd5945056cdbd4f4a2ca36
jakrzus <jakub.krzus@gmail.com>
2024-10-10 21:32:55 +0200

    [create] TripSerializer

---
+++ app/serializers/trip_serializer.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: trips
+#
+#  id                  :bigint           not null, primary key
+#  date                :date
+#  destination_address :string
+#  distance            :integer
+#  price               :decimal(8, 2)
+#  start_address       :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+class TripSerializer < ActiveModel::Serializer
+  attributes :id, :start_address, :destination_address, :price, :date, :distance
+
+  def date
+    month = I18n.l(object.date, format: :month)
+    day = object.date.day.ordinalize
+    "#{month}, #{day}"
+  end
+
+  def distance
+    km = (object.distance / 1000.0).round(2)
+
+    "#{km} km"
+  end
+end
